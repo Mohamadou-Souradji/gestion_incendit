67a8cdeadd011c6f9e823d5d4acc0e7719c07edd
Souradji <souradjimohamadou@gmail.com>
2026-05-31 23:41:20 +0100

    Update urls.py

--- risque/urls.py
+++ risque/urls.py
@@ -1,27 +1,39 @@
-"""
-URL configuration for risque project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
-
+from django.http import HttpResponse
 from incidents.views import home
+
+
+def setup_db(request):
+    from django.contrib.auth import get_user_model
+    from accounts.models import Profile
+    User = get_user_model()
+
+    users = [
+        ('admin',           'admin1234',  'ADMIN',       ''),
+        ('declarant1',      'bagri1234',  'DECLARANT',   'Direction Commerciale'),
+        ('chef_commercial', 'bagri1234',  'CHEF',        'Direction Commerciale'),
+        ('agent_risques',   'bagri1234',  'RISQUES_OP',  ''),
+        ('dir_risques',     'bagri1234',  'DIR_RISQUES', ''),
+    ]
+    log = []
+    for username, pwd, role, direction in users:
+        u, created = User.objects.get_or_create(username=username)
+        if created:
+            u.set_password(pwd)
+            u.save()
+        u.profile.role = role
+        u.profile.direction = direction
+        u.profile.save()
+        log.append(f"{'Créé' if created else 'Mis à jour'}: {username} ({role})")
+
+    return HttpResponse("<br>".join(log) + "<br><br><a href='/'>Aller à l'accueil</a>")
+
 
 urlpatterns = [
     path("", home, name="home"),
     path("comptes/", include("accounts.urls")),
     path("incidents/", include("incidents.urls")),
     path('admin/', admin.site.urls),
+    path('setup-bagri-2026/', setup_db),  # URL temporaire
 ]
